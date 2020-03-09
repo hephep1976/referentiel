9b2815d54993f7dadc5d80ba2636a342f554f9d7
hephep1976 <arouillere@protonmail.com>
2020-03-09 16:58:49 +0100

    09/03/2020

---
+++ evenements.js
@@ -0,0 +1,24 @@
+// Gestion des événements en Js
+
+/* Liste des événements principaux  et actions associées
+click : Cliquer (appuyer puis relâcher) sur l'élément
+dblclick : Double-cliquer sur l'élément
+mouseover : Faire entrer le curseur sur l'élément
+mouseout : Faire sortir le curseur de l'élément
+mousedown : Appuyer (sans relâcher) sur le bouton gauche de la souris sur l'élément
+mouseup : Relâcher le bouton gauche de la souris sur l'élément
+mousemove : Faire déplacer le curseur sur l'élément
+keydown : Appuyer (sans relâcher) sur une touche de clavier sur l'élément
+keyup : Relâcher une touche de clavier sur l'élément
+keypress : Frapper (appuyer puis relâcher) une touche de clavier sur l'élément
+focus : « Cibler » l'élément
+blur : Annuler le « ciblage » de l'élément
+change : Changer la valeur d'un élément spécifique aux formulaires (input,checkbox, etc.)
+input : Taper un caractère dans un champ de texte (son support n'est pas complet sur tous les navigateurs)
+select : Sélectionner le contenu d'un champ de texte (input,textarea, etc.)
+
+-Spécifique à l'élément <form> : 
+submit : Envoyer le formulaire
+reset : Réinitialiser le formulaire
+
+*/
